e8b32270f4f7af20826c3d7205cba87b25dfaea9
Pedro <Pedro@Pedro-Desktop>
2018-02-01 10:22:26 +0100

    Root Commit

---
+++ src/Basicos.java
@@ -0,0 +1,24 @@
+
+public class Basicos {
+
+	public static void main(String[] args) {
+		//Enteros
+		byte numByte = -128;
+		short numShort = 32000;
+		int numInt = 1234567890;
+		long numLong = 1234567890123456789L;
+
+		//Caracter
+		char letraChar = 'c';
+		
+		//Reales
+		float numFloat = 1.23F;
+		double numDouble = 1E6;
+		
+		//lógico
+		boolean condicionBoolean=false;
+		
+		System.out.println("El valor es: " + condicionBoolean);
+	}
+
+}
